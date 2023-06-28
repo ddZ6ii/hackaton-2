d97e02faa5ea9e4346a34357185b18dae5585ed0
Denis Dezest <denis.dezest@gmail.com>
2023-06-28 18:06:06 +0200

    Completed databse population

--- backend/src/database/database.sql
+++ backend/src/database/database.sql
@@ -54,13 +54,6 @@ CREATE TABLE `phone_feature` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 -- ___________________________________ POPULATE TABLES ___________________________________ 
-
--- Create user
-INSERT INTO `user` (`email`, `password`, `is_admin`) 
-VALUES 
-('admin@gmail.com', 'admin', 'true'),
-('user@gmail.com', 'user', 'false');
-
 -- Create center
 INSERT INTO `center` (`city`, `region`, `ZIP`) 
 VALUES 
@@ -71,15 +64,50 @@ VALUES
 ('Bordeaux', 'Nouvelle-Aquitaine', 33800),
 ('Marseille', "Provence-Alpes-Cote d'Azur", 13001);
 
+-- Create user
+INSERT INTO `user` (`email`, `password`, `is_admin`, `center_id`) 
+VALUES 
+('admin@gmail.com', 'admin', true, 3),
+('user@gmail.com', 'user', false, 1);
+
 -- Create phone
 INSERT INTO `phone` (`brand`, `model`, `category`, `price`, `thumbnail_1`, `thumbnail_2`, `thumbnail_3`, `creation_date`, `center_id`) 
 VALUES 
-('iPhone', 'iPhone10', '5-Premium', 189.19, 'de0aedaf-9ff1-4622-8176-715b8b3f1a5b-iPhone10.jpg', '', '', '')
-
--- Create phone_feature
-
+('Apple', 'iPhone11', '5-Premium', 189.19, './assets/image/iphone-11.jpg', '', '', '2023-06-26', 1),
+('Samsung', 'Galaxy S10', '5-Premium', 209.19, './assets/image/iphone-13.jpg', '', '', '2023-06-28', 3),
+('Apple', 'iPhone14', '5-Premium', 656.19, './assets/image/iphone-14.jpg', '', '', '2023-06-28', 2),
+('Google', 'pixel6', '3-B', 89.25, './assets/image/google-pixel-6a.jpg', '', '', '2023-06-28', 4),
+('Nokia', '3310', '2-C', 42, './assets/image/iphone-13.jpg', '', '', '2023-02-08', 3),
+('Huawei', 'Huawei Mate50 Pro', '1-HC', 0, './assets/image/huawei-mate-50p.jpg', '', '', '2023-05-24', 1),
+('Xiaomi', 'Redmi Note 10 Pro', '4-A', 169.78, './assets/image/redmi-note-7.webp', '', '', '2023-05-24', 6),
+('Oppo', 'Reno 7', '2-B', 124, './assets/image/oppo-reno-7.jpg', '', '', '2023-07-12', 1),
+('Xiaomi', 'Redmi Note 11', '1-HC', 0, './assets/image/redmi-note-11.jpg', '', '', '2023-01-24', 5),
+('Apple', 'Iphone 13', '1-HC', 0, './assets/image/iphone-13.jpg', '', '', '2023-07-01', 1);
 
 -- Create feature
 INSERT INTO `feature` (`has_charger`, `state`, `OS`, `RAM`, `storage`, `color`, `screen`, `network`) 
 VALUES 
-('', '', '', '', '', '', '', '')
+(1, 'RECONDITIONNE', 'iOS', 2, 32, 'argent', '5.8', '4G'),
+(0, 'RECONDITIONNE', 'Android', 8, 64, 'noir', '6.1', '4G'),
+(1, 'RECONDITIONNABLE', 'iOS', 2, 32, 'argent', '5.8', '4G'),
+(0, 'BLOQUE', 'iOS', 2, 32, 'argent', '5.8', '4G'),
+(1, 'REPARABLE', 'iOS', 2, 32, 'noir', '5.8', '4G'),
+(0, 'DEEE', 'iOS', 2, 32, 'argent', '5.8', '4G'),
+(1, 'BLOQUE', 'iOS', 2, 32, 'noir', '5.8', '4G'),
+(0, 'DEEE', 'iOS', 2, 32, 'argent', '5.8', '4G'),
+(1, 'DEEE', 'android', 2, 32, 'noir', '5.8', '4G'),
+(0, 'DEEE', 'iOS', 2, 32, 'argent', '5.8', '4G');
+
+-- Create phone_feature
+INSERT INTO `phone_feature` (`phone_id`, `feature_id`) 
+VALUES 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10);
