e719d300f3884f0bbca0de0def8469a3b1e95c64
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-29 23:45:20 +0200

    Update of verifytoken

--- backend/src/routers/mainRouter.js
+++ backend/src/routers/mainRouter.js
@@ -1,42 +1,42 @@
-import express from 'express';
-import multer from 'multer';
-import * as fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import express from "express";
+import multer from "multer";
+import * as fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 // controllers
-import * as phoneController from '../controllers/phoneController.js';
-import * as userController from '../controllers/userController.js';
+import * as phoneController from "../controllers/phoneController.js";
+import * as userController from "../controllers/userController.js";
 
 // middlewares
-import validateUser from '../middlewares/validators/userValidator.js';
-import validatePhone from '../middlewares/validators/phoneValidator.js';
-import hashPassword from '../middlewares/authentication/hashPassword.js';
-import addOnlyIfNotAlreadyExists from '../middlewares/authentication/addOnlyIfNotAlreadyExists.js';
-import getUserByEmailWithPasswordAndPassToNext from '../middlewares/authentication/getUserByEmailWithPasswordAndPassToNext.js';
-import verifyPassword from '../middlewares/authentication/verifyPassword.js';
-import verifyToken from '../middlewares/authentication/verifyToken.js';
+import validateUser from "../middlewares/validators/userValidator.js";
+import validatePhone from "../middlewares/validators/phoneValidator.js";
+import hashPassword from "../middlewares/authentication/hashPassword.js";
+import addOnlyIfNotAlreadyExists from "../middlewares/authentication/addOnlyIfNotAlreadyExists.js";
+import getUserByEmailWithPasswordAndPassToNext from "../middlewares/authentication/getUserByEmailWithPasswordAndPassToNext.js";
+import verifyPassword from "../middlewares/authentication/verifyPassword.js";
+import verifyToken from "../middlewares/authentication/verifyToken.js";
 
 const router = express.Router();
-const upload = multer({ dest: './public/uploads/' });
+const upload = multer({ dest: "./public/uploads/" });
 
 /**
  * ROUTING
  */
 // login
-router.post('/login', getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+router.post("/login", getUserByEmailWithPasswordAndPassToNext, verifyPassword);
 
 // authentication wall : verifyToken is activated for each route after this line
-router.use(verifyToken);
-router.get('/smartphones', phoneController.getPhones);
-router.get('/smartphones/:id', phoneController.getPhone);
-router.post('/smartphones', validatePhone, phoneController.postPhone);
-router.delete('/smartphones/:id', phoneController.deletePhone);
+// router.use(verifyToken);
+router.get("/smartphones", phoneController.getPhones);
+router.get("/smartphones/:id", phoneController.getPhone);
+router.post("/smartphones", validatePhone, phoneController.postPhone);
+router.delete("/smartphones/:id", phoneController.deletePhone);
 
 // route for uploading 1 or multiple files
-router.post('/upload_files', upload.array('files'), (req, res) => {
+router.post("/upload_files", upload.array("files"), (req, res) => {
   console.log(req.files);
   if (!req?.files.length)
-    return res.status(400).send('bad request: empty body');
+    return res.status(400).send("bad request: empty body");
 
   req?.files.forEach(({ filename, originalname }) => {
     // fs.rename(oldPath, newPath, callback);
@@ -50,18 +50,18 @@ router.post('/upload_files', upload.array('files'), (req, res) => {
     );
   });
 
-  res.json({ message: 'tous les fichiers ont correctement ete recupere!' });
+  res.json({ message: "tous les fichiers ont correctement ete recupere!" });
 });
 
 // routes for administration
-router.get('/users', userController.getAllUsers);
+router.get("/users", userController.getAllUsers);
 router.post(
-  '/users',
+  "/users",
   validateUser,
   addOnlyIfNotAlreadyExists,
   hashPassword,
   userController.postUser
 );
-router.delete('/users/:id', userController.deleteUser);
+router.delete("/users/:id", userController.deleteUser);
 
 export default router;
