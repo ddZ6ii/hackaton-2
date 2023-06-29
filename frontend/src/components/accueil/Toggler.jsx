2977608e5831ce65bc5b5148767cdbcd802101c9
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-29 10:19:34 +0200

    Updated Toggler.jsx

--- frontend/src/components/accueil/Toggler.jsx
+++ frontend/src/components/accueil/Toggler.jsx
@@ -72,7 +72,7 @@ export default function Connect() {
             </h2>
             <button
               type="button"
-              className="connect-button connect-ghostButton flex items-center gap-2 text-primary"
+              className="connect-button connect-ghostButton flex items-center gap-2"
               onClick={() => toggle(true)}
             >
               Vers le catalogue de téléphone
@@ -110,7 +110,7 @@ export default function Connect() {
 
             <button
               type="button"
-              className="connect-button connect-ghostButton flex items-center gap-2 text-primary"
+              className="connect-button connect-ghostButton flex items-center gap-2"
               onClick={() => toggle(false)}
             >
               <svg
