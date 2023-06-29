33813f6d6e87ec09ea270eec430311e45beb6f47
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-06-29 16:51:18 +0200

    fetched data

--- frontend/src/pages/PhoneDetails.jsx
+++ frontend/src/pages/PhoneDetails.jsx
@@ -1,25 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import React, { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 
+import styles from "../css/PhoneDetails.module.css"
+
 export default function PhoneDetails() {
+  const [phone, setPhone] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
   const { id } = useParams();
 
-  const slides = [
-    {
-      url: "../../public/assets/image/iphone-13.jpg",
-    },
-    {
-      url: "../../public/assets/image/test.png",
-    },
-    {
-      url: "//images.samsung.com/fr/smartphones/galaxy-z-flip4/buy/03_Product-Image_Basic-Color/4_B4_KV_MO_Blue.jpg",
-    },
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    axios.get("http://localhost:5000/smartphones/").then((res) => {
+      const phones = res.data;
+      const filteredPhone = phones.filter((phone) => phone.id_phone == id);
+      console.log(filteredPhone[0]);
+      setPhone(filteredPhone[0]);
+    });
+  }, []);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -37,30 +37,46 @@ export default function PhoneDetails() {
     setCurrentIndex(slideIndex);
   };
 
+  useEffect(() => {
+    if (phone.thumbnail_1 && phone.thumbnail_2 && phone.thumbnail_3) {
+      setCurrentIndex(0);
+    }
+  }, [phone]);
+
+  const imagePath = "../public/assets/image/";
+
+  const slides = [
+    `${imagePath}${phone.thumbnail_1}`,
+    `${imagePath}${phone.thumbnail_2}`,
+    `${imagePath}${phone.thumbnail_3}`,
+  ];
+
   return (
-    <div className="container">
-      <h2 className="mt-12 text-center">Apple Iphone 10 {}</h2>
-      <div className="features flex justify-around">
-        <div className="">
-          <h3>Caractéristiques techniques</h3>
-          <p className="my-4">Appareil : {} </p>
-          <p className="mb-4">Marque : {} </p>
-          <p className="mb-4">Modèle : {} </p>
-          <p className="mb-4">Taille d'écran : {} </p>
-          <p className="mb-4">Taile de la RAM : {} </p>
-          <p className="mb-4">Taille de l'espace de stockage : {} </p>
-          <p> Version du système d'exploitation : {} </p>
+    <section className={`${styles.phone_details_container} sm:flex sm:flex-row justify-center items-center`}>
+      <div>
+        <h2 className="mb-4 mt-12 text-center">
+          {phone.brand} {phone.model}
+        </h2>
+        <div className="features flex justify-around">
+          <div className="flex flex-col">
+            <h3>Caractéristiques techniques</h3>
+            <p className="mb-4">Marque : {phone.brand} </p>
+            <p className="mb-4">Modèle : {phone.model} </p>
+            <p className="mb-4">Taille d'écran : {phone.screen} pouces </p>
+            <p className="mb-4">Taile de la RAM : {phone.RAM} Go </p>
+            <p className="mb-4">
+              Taille de l'espace de stockage : {phone.storage} Go{" "}
+            </p>
+            <p> Système d'exploitation : {phone.OS} </p>
+          </div>
         </div>
       </div>
-      {/* <div className="group relative flex h-[780px] w-full max-w-[1400px] flex-col items-center justify-center px-4 pt-8">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="h-3/5 w-2/4 rounded-2xl bg-cover bg-center duration-500"
-        ></div>
-        <div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+      <div className={`group relative mt-8 flex w-1/2 flex-col items-center justify-center`}>
+        <img src={slides[currentIndex]} className="h-96 rounded-xl" />
+        <div className={`${styles.chevron_icon} absolute left-2 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block`}>
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
-        <div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+        <div className="chevron-icon absolute right-2 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
         <div className="top-4 flex justify-center py-2">
@@ -68,36 +84,15 @@ export default function PhoneDetails() {
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
-              className="cursor-pointer text-2xl"
+              className={`cursor-pointer text-2xl ${
+                slideIndex === currentIndex ? "text-primary" : ""
+              }`}
             >
               <RxDotFilled />
             </div>
           ))}
         </div>
-      </div> */}
-      <div className="group relative flex h-screen w-screen max-w-[1400px] flex-col items-center justify-center ">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="h-3/5 w-2/4 rounded-2xl bg-cover bg-center duration-500"
-        ></div>
-        <div className="chevron-icon absolute left-5 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
-        <div className="chevron-icon absolute right-5 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className="top-4 flex justify-center py-2">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className={`cursor-pointer text-2xl ${slideIndex === currentIndex ? 'text-primary' : ''}`}
-              >
-              <RxDotFilled />
-            </div>
-          ))}
-        </div>
       </div>
-    </div>
+    </section>
   );
 }
