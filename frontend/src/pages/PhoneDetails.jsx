cef220b07ceb3b3f8491becc6dd63c31315d997e
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-06-29 14:33:04 +0200

    changed hover color of icons

--- frontend/src/pages/PhoneDetails.jsx
+++ frontend/src/pages/PhoneDetails.jsx
@@ -39,22 +39,20 @@ export default function PhoneDetails() {
 
   return (
     <div className="container">
-      <h2 className="mt-4 text-center">Apple Iphone 10 {}</h2>
-      <div>
-        <div className="image-features flex justify-around">
-          <div className="">
-            <h3>Caractéristiques techniques</h3>
-            <p className="my-4">Appareil : {} </p>
-            <p className="mb-4">Marque : {} </p>
-            <p className="mb-4">Modèle : {} </p>
-            <p className="mb-4">Taille d'écran : {} </p>
-            <p className="mb-4">Taile de la RAM : {} </p>
-            <p className="mb-4">Taille de l'espace de stockage : {} </p>
-            <p> Version du système d'exploitation : {} </p>
-          </div>
+      <h2 className="mt-12 text-center">Apple Iphone 10 {}</h2>
+      <div className="features flex justify-around">
+        <div className="">
+          <h3>Caractéristiques techniques</h3>
+          <p className="my-4">Appareil : {} </p>
+          <p className="mb-4">Marque : {} </p>
+          <p className="mb-4">Modèle : {} </p>
+          <p className="mb-4">Taille d'écran : {} </p>
+          <p className="mb-4">Taile de la RAM : {} </p>
+          <p className="mb-4">Taille de l'espace de stockage : {} </p>
+          <p> Version du système d'exploitation : {} </p>
         </div>
       </div>
-      <div className="group relative flex h-[780px] w-full max-w-[1400px] flex-col items-center px-4 pt-16">
+      {/* <div className="group relative flex h-[780px] w-full max-w-[1400px] flex-col items-center justify-center px-4 pt-8">
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
           className="h-3/5 w-2/4 rounded-2xl bg-cover bg-center duration-500"
@@ -72,6 +70,29 @@ export default function PhoneDetails() {
               onClick={() => goToSlide(slideIndex)}
               className="cursor-pointer text-2xl"
             >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+      </div> */}
+      <div className="group relative flex h-screen w-screen max-w-[1400px] flex-col items-center justify-center ">
+        <div
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          className="h-3/5 w-2/4 rounded-2xl bg-cover bg-center duration-500"
+        ></div>
+        <div className="chevron-icon absolute left-5 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        <div className="chevron-icon absolute right-5 top-[50%] -translate-x-0 -translate-y-1/2 cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className="top-4 flex justify-center py-2">
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className={`cursor-pointer text-2xl ${slideIndex === currentIndex ? 'text-primary' : ''}`}
+              >
               <RxDotFilled />
             </div>
           ))}
