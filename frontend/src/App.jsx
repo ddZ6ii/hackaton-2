bd01ff672181b290f932c0ef34c5429ba764f69d
Denis Dezest <denis.dezest@gmail.com>
2023-06-30 01:28:38 +0200

    Fixed mobile min h screen by setting a footer height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import { useIdentification } from './hooks/useIdentification.js';
 
 // Pages
-import Home from "./pages/Home";
-import Accueil from "./pages/Accueil";
-import Faq from "./pages/Faq";
-import Account from "./pages/Account";
-import NotFound from "./pages/NotFound";
-import PhoneDetails from "./pages/PhoneDetails";
+import Home from './pages/Home';
+import Accueil from './pages/Accueil';
+import Faq from './pages/Faq';
+import Account from './pages/Account';
+import NotFound from './pages/NotFound';
+import PhoneDetails from './pages/PhoneDetails';
 
 // Components
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 export default function App() {
+  const { isLoggedIn } = useIdentification();
   return (
     <>
       <NavBar />
-      <main className="min-h-[calc(100dvh-100px)] pt-10 md:min-h-[calc(100dvh-40px)] md:pt-20">
+      <main className='min-h-[calc(100dvh-88px)] pt-10 md:min-h-[calc(100dvh-40px)] md:pt-20'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/accueil" element={<Accueil />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/phones/:id" element={<PhoneDetails />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/accueil' element={<Accueil />} />
+          <Route path='/faq' element={<Faq />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/phones/:id' element={<PhoneDetails />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
