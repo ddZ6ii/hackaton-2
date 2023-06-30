c337d161abbf069db7c4efa8994eee6a61e9d197
Denis Dezest <denis.dezest@gmail.com>
2023-06-30 06:51:59 +0200

    enhanced FAQ page

--- frontend/src/components/RowQuestion.jsx
+++ frontend/src/components/RowQuestion.jsx
@@ -1,14 +1,16 @@
-import "../App.css";
+import '../App.css';
 
 const RowQuestion = ({ faq, index, toggleFAQ }) => {
   return (
     <div
-      className={"faq " + (faq.open ? "open" : "")}
+      className={'faq ' + (faq.open ? 'open' : '')}
       key={index}
       onClick={() => toggleFAQ(index)}
     >
-      <div className="faq-question">{faq.question}</div>
-      <div className="faq-answer">{faq.answer}</div>
+      <div className='faq-question'>{faq.question}</div>
+      <div className='faq-answer rounded-full bg-neutralLight/80 px-8 py-4 text-neutralDark'>
+        {faq.answer}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -1,40 +1,12 @@
-import { useState } from "react";
-import "../App.css";
-import RowQuestion from "../components/RowQuestion";
+import { useState } from 'react';
+import RowQuestion from '../components/RowQuestion';
+
+import '../App.css';
+
+import faqData from '../data/faq.json';
+
 export default function Faq() {
-  const [faqs, setFaqs] = useState([
-    {
-      question: "Comment puis-je me connecter?",
-      answer:
-        "Il suffit d'appuyer sur 'Se connecter' et de rentrer les informations demandées par votre centre.",
-      open: true,
-    },
-    {
-      question:
-        "Quelles sont les démarques à effectuer pour un téléphone qui ne fonctionne pas?",
-      answer:
-        "Vous pouvez contacter le service client ou voir votre réferent de centre.",
-      open: false,
-    },
-    {
-      question: "Pouvons-nous régler le télèphone en plusieurs fois?",
-      answer:
-        "Il est possible de régler en plusieurs fois, voir les modalités auprès de votre réferent.",
-      open: false,
-    },
-    {
-      question:
-        "Notre recherche sera-t-elle mémoriser à chaque connexion sur l'application?",
-      answer:
-        "Un historique de commande peut être effectuée, mais en faire la demande.",
-      open: false,
-    },
-    {
-      question: "Peut-on être livré directement à son domicile?",
-      answer: "Ces modalités sont à en discuter avec le réferent du centre.",
-      open: false,
-    },
-  ]);
+  const [faqs, setFaqs] = useState(faqData);
 
   const toggleFAQ = (index) => {
     setFaqs(
@@ -44,16 +16,15 @@ export default function Faq() {
         } else {
           faq.open = false;
         }
-
         return faq;
       })
     );
   };
 
   return (
-    <section className="flex items-center justify-center">
-      <h2 className="text-xl uppercase">FAQ</h2>
-      <div className="faqs">
+    <section className='flex items-center justify-center'>
+      <h2 className='text-xl uppercase'>FAQ</h2>
+      <div className='faqs'>
         {faqs.map((faq, index) => (
           <RowQuestion
             faq={faq}
@@ -63,8 +34,8 @@ export default function Faq() {
           />
         ))}
         <img
-          className="absolute bottom-0 right-0 -z-10 w-[500px]"
-          src="../assets/icons/logo.svg"
+          className='absolute bottom-0 right-0 -z-10 w-[500px]'
+          src='../assets/icons/logo.svg'
         ></img>
       </div>
     </section>
