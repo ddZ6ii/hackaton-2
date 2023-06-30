83a80df9fcad667a34a272f958ad61b996f28a51
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-06-30 02:55:14 +0200

    Update of table component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
-import { useIdentification } from './hooks/useIdentification.js';
+import { Routes, Route } from "react-router-dom";
+import { useIdentification } from "./hooks/useIdentification.js";
 
 // Pages
-import Home from './pages/Home';
-import Accueil from './pages/Accueil';
-import Faq from './pages/Faq';
-import Account from './pages/Account';
-import NotFound from './pages/NotFound';
-import PhoneDetails from './pages/PhoneDetails';
+import Home from "./pages/Home";
+import Accueil from "./pages/Accueil";
+import Faq from "./pages/Faq";
+import Account from "./pages/Account";
+import NotFound from "./pages/NotFound";
+import PhoneDetails from "./pages/PhoneDetails";
 
 // Components
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from "./components/utilities/NavBar";
+import Footer from "./components/utilities/Footer";
 
 export default function App() {
   const { isLoggedIn } = useIdentification();
   return (
     <>
       <NavBar />
-      <main className='min-h-[calc(100dvh-45px)] pt-24 md:min-h-[calc(100vh-134px)] md:pt-0'>
+      <main className="min-h-[calc(100dvh-45px)] pt-24 md:min-h-[calc(100vh-134px)] md:pt-0">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/accueil' element={<Accueil />} />
-          <Route path='/faq' element={<Faq />} />
-          <Route path='/account' element={<Account />} />
-          <Route path='/phones/:id' element={<PhoneDetails />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/phones/:id" element={<PhoneDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <section>
-      <h1>Home</h1>
+      <h1 className="uppercase">Bienvenue</h1>
+      <img
+        className="absolute bottom-0 right-0 flex w-[500px]"
+        src="../assets/icons/logo.svg"
+      ></img>
     </section>
   );
 }
